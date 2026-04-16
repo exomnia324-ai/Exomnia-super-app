f4ca55bcbab362bd180fd6fe77f65bed9d3b6698
Devasis0055 <devasisadhikary175@gmail.com>
2026-04-16 18:28:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,5 @@ app.route("/")
 def home():
     return render_template("index.html")
 
-if __name__ == "__main__":
-    app.run(host=0.0.0.0,port=10000)
+if __name__ == __main__:
+    app.run(host="0.0.0.0",port="10000")
